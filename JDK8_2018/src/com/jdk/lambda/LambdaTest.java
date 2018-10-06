bfc3fe11fe6047e3a63419fb2b6ff23194298871
lisangCHN <1186733841@qq.com>
2018-10-06 15:12:51 +0800

    jdk 1.8 新特性 -> lambda表达式的使用

---
+++ JDK8_2018/src/com/jdk/lambda/LambdaTest.java
@@ -0,0 +1,13 @@
+package com.jdk.lambda;
+
+/**
+ * JDK 1.8 -> lambda表达式的使用
+ * Create by lisang on 2018/10/6.
+ */
+public class LambdaTest {
+
+    public static void main(String[] args){
+
+    }
+
+}
